c69511f97aaf192c5dacbb06302e9c6d2c804f3f
jumanah <jumanah.f.14@gmail.com>
2023-08-21 14:24:18 +0300

    add

---
+++ index.js
@@ -0,0 +1,12 @@
+// first exercise
+function javascript_Boot_camp(teach1,teach2){
+    alert("This javaScript BootCamp with" +teach1+"ok")
+}
+javascript_Boot_camp("teach1","teach2")
+
+/////////////////////
+// Second exercise
+function name(num1,num2,) {
+    alert((num1 + num2)/2)
+}
+average(10,5)
